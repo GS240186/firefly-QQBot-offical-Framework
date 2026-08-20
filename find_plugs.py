2012fe4978e4f97acec591b4ee4893f3297e8f55
Firefly <3138158116@qq.com>
2026-08-20 15:02:36 +0800

    Delete find_plugs.py

--- find_plugs.py
+++
@@ -1,7 +0,0 @@
-with open(r'd:\小流萤bot\console_server.py','r',encoding='utf-8') as f:
-    t = f.read()
-import re
-for m in re.finditer(r'def set_feature_enabled_global|def is_feature_enabled|set_feature_enabled\b', t):
-    s = m.start()
-    print(s, '->', t[max(0,s-80):s+200].replace('\n', ' | '))
-    print('---')
